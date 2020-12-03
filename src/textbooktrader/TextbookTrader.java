80309dc14d872d9ee91596465eb5aa4dbacb40fb
jpd5749 <jpd5749@psu.edu>
2020-12-03 15:18:07 -0500

    Updated the names of the files. Removed many of the unused model classes.

--- src/textbooktrader/TextbookTrader.java
+++ src/textbooktrader/TextbookTrader.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package jonasjasonfxml;
+package textbooktrader;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author Jonas
  */
-public class JonasJasonFXML extends Application {
+public class TextbookTrader extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
