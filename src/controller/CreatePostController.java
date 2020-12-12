1136f5228a6869074a8445d97bf8086781efea5f
katiebridge3 <64429752+katiebridge3@users.noreply.github.com>
2020-12-12 10:52:18 -0500

    create post controller setup

---
+++ src/controller/CreatePostController.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author katie
+ */
+public class CreatePostController {
+    
+    @FXML
+    private TextField materialName;
+
+    @FXML
+    private TextField courseName;
+
+    @FXML
+    private TextField sellerEmail;
+
+    @FXML
+    private TextField materialCondition;
+
+    @FXML
+    private TextField isbnBook;
+
+    @FXML
+    void backToLogin(ActionEvent event) {
+
+    }
+
+    @FXML
+    void createPost(ActionEvent event) {
+
+    }
+    
+}
